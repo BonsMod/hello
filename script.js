f49a9f211b70e36f565d8e8cae0efd5ecd975756
bonsmod <bonsmod@gmail.com>
2017-03-05 14:16:30 +0100

    Amélioration de la fonction <bienvenue> pour qu'elle récupére le non saisie par l'utilisateur et lui souhaiter la bienvenue et l'inviter à saisir son nom s'il ne l'a pas fait

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 
 function bienvenue(){
-	alert("Bienvenue")
-;}
+	var nom = document.getElementsByName("nom")[0].value;
+	if(nom)
+		alert("Bonjour " + nom + ", bienvenue sur notre site");
+	else
+		alert("Veuillez saisir votre nom et recliquer sur le boutton");
+}
